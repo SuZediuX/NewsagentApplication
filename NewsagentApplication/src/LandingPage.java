2a8dedfdbcbd2f04545fbff22568b4c679f58e17
a00279807 <a00279807@student.ait.ie>
2022-02-06 17:43:34 +0000

    Added a simple menu | Added DatabaseHandler | Menu needs improvement

--- NewsagentApplication/src/LandingPage.java
+++ NewsagentApplication/src/LandingPage.java
@@ -1,9 +1,38 @@
+import java.util.Scanner;
 
 public class LandingPage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("");
+		System.out.println("--- NewsAgent's Menu ---");
+		System.out.println("1. Customers");
+		System.out.println("2. Subscriptions");
+		System.out.println("3. Deliveries");
+		System.out.println("4. Payments");
+		System.out.println("--- Waiting for input ---");
+		
+		Scanner input = new Scanner(System.in);
+		String data = input.nextLine();
+		
+		if(Integer.parseInt(data)<1||Integer.parseInt(data)>4) {
+			System.out.println("Expected input from 1 to 4");
+			LandingPage.main(args);
+		}
+		else {
+			switch(Integer.parseInt(data)) {
+			case 1: System.out.println("Okay! Passing Control to Customer Class!");
+			break;
+			case 2: System.out.println("Okay! Passing Control to Subscription Class!");
+			break;
+			case 3: System.out.println("Okay! Passing Control to Delivery Class!");
+			break;
+			case 4: System.out.println("Okay! Passing Control to Payment Class!");
+			break;
+			default: System.out.println("Illegal argument! This message must not be invokeable!");
+			break;
+			}
+		}
+		
 	}
 
 }
