d89e04cec51097488cfedf72462f5370197babc9
a00279807 <a00279807@student.ait.ie>
2022-02-07 22:21:55 +0000

    Added PaymentHandler method signature | Added PaymentHandlerTest class

---
+++ NewsagentApplication/src/PaymentHandler.java
@@ -0,0 +1,19 @@
+
+public class PaymentHandler {
+	//Add new payment record
+	public void addNewPay(Customer customer, PaymentHandler payMethod) {
+		
+	}
+	//View existing payment record
+    public void viewPay(int id) {
+		
+	}
+	//Modify existing payment record
+	public void updatePay(Customer customer) {
+		
+	}
+	//Purge existing payment record
+    public void deletePay(int id) {
+		
+	}
+}
---
+++ NewsagentApplication/src/PaymentHandlerTest.java
@@ -0,0 +1,28 @@
+import junit.framework.TestCase;
+
+public class PaymentHandlerTest extends TestCase {
+    //Test No: 1
+	//Objective: To verify if the entered payment ID contains 3 to 6 valid characters between 0 to 9
+	
+	public void testvalidatePaymentID001() {
+		try {
+			
+		}
+		catch(Exception e){
+			
+		}
+		
+	}
+	//Test No: 2
+	//Objective: To verify if the entered payment method contains one among the following of the values: cash, card
+	
+	public void testvalidatePaymentID002() {
+		try {
+			
+		}
+		catch(Exception e){
+			
+		}
+		
+	}
+}
