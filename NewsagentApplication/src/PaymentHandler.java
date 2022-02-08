be58856b47dd95484e61ab7739dbd36519ee5317
a00279807 <a00279807@student.ait.ie>
2022-02-08 11:08:31 +0000

    Added RuntimeException to PaymentHandler method signature

--- NewsagentApplication/src/PaymentHandler.java
+++ NewsagentApplication/src/PaymentHandler.java
@@ -1,19 +1,19 @@
 
 public class PaymentHandler {
 	//Add new payment record
-	public void addNewPay(Customer customer, PaymentHandler payMethod) {
-		
+	public void addNewPay(String customer, String payMethod) {
+		throw new RuntimeException("No code written");
 	}
 	//View existing payment record
     public void viewPay(int id) {
-		
+		throw new RuntimeException("No code written");
 	}
 	//Modify existing payment record
-	public void updatePay(Customer customer) {
-		
+	public void updatePay(int id) {
+		throw new RuntimeException("No code written");		
 	}
 	//Purge existing payment record
     public void deletePay(int id) {
-		
+    	throw new RuntimeException("No code written");
 	}
 }
