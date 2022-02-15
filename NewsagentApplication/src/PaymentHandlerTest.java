56ab13edad7e95e07d37751e4c1e902fd38589ec
a00279807 <a00279807@student.ait.ie>
2022-02-15 11:03:33 +0000

    Added 1 test case | All passing

--- NewsagentApplication/src/PaymentHandlerTest.java
+++ NewsagentApplication/src/PaymentHandlerTest.java
@@ -2,7 +2,7 @@ import junit.framework.TestCase;
 
 public class PaymentHandlerTest extends TestCase {
     //Test No: 1
-	//Objective: To test a valid ID and method input
+	//Objective: To test a valid ID and method input | ID BVA #1/4 | Payment Method EP #1/2 | Payment Method CT #1/3
 	
 	public void testvalidatePayment001() {
 		try {
@@ -12,11 +12,9 @@ public class PaymentHandlerTest extends TestCase {
 		} catch (PaymentHandlerExceptionHandler e) {
 			fail("Exception not expected");
 		}
-		
-		
 	}
 	//Test No: 2
-	//Objective: To test an invalid boundary ID below 100
+	//Objective: To test an invalid boundary ID below 100 | ID BVA #2/4 | ID EP #2/3
 	
 	public void testvalidatePayment002() {
 		try {
@@ -26,12 +24,26 @@ public class PaymentHandlerTest extends TestCase {
 		catch(PaymentHandlerExceptionHandler e){
 			assertEquals("Payment ID below expected range", e.getMessage());
 		}
-		
 	}
 	//Test No: 3
-		//Objective: To test an invalid method input
+	//Objective: To test an invalid method input | Payment Method BVA #1 | Payment Method EP #2/2 | Payment Method CT #2/3
 		
 		public void testvalidatePayment003() {
+			try {
+				PaymentHandler testObject = new PaymentHandler(999999, "000");
+				assertEquals(999999, testObject.getID());
+				testObject.validateMethod("000");
+				fail("Exception expected");
+			}
+			catch(PaymentHandlerExceptionHandler e){
+				assertEquals("Input not in either expected category", e.getMessage());
+			}
+		}
+		
+	//Test No: 4
+	//Objective: To test a valid ID and an invalid method input
+		
+		public void testvalidatePayment004() {
 			try {
 				PaymentHandler.validateMethod("Cheque");
 				fail("Exception expected");
@@ -39,7 +51,6 @@ public class PaymentHandlerTest extends TestCase {
 			catch(PaymentHandlerExceptionHandler e){
 				assertEquals("Input not in either expected category", e.getMessage());
 			}
-			
 		}
 }
 
