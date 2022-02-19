9e18e1be6b49627e96a264f0046641e1dbbceedf
a00279807 <a00279807@student.ait.ie>
2022-02-19 19:46:17 +0000

    Added new Main class CommandLine | LandingPage deprecated

---
+++ NewsagentApplication/src/CommandLine.java
@@ -0,0 +1,25 @@
+
+public class CommandLine {
+	
+	private static void listCustomerMenu() {
+		
+	}
+	
+    private static void listSubscriptionMenu() {
+		
+	}
+
+    private static void listDeliveryMenu() {
+	
+}
+
+    private static void listPaymentMenu() {
+	
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
