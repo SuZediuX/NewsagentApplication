fc9fbd329b46363bf71041e7e4e1064d27018a5c
a00279807 <a00279807@student.ait.ie>
2022-02-23 21:52:34 +0000

    Removed redundant classes | Recommit

--- NewsagentApplication/src/CommandLine.java
+++ NewsagentApplication/src/CommandLine.java
@@ -1,25 +1,130 @@
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.util.Scanner;
 
 public class CommandLine {
 	
+	private static void listMainMenu() {
+		System.out.println("Choose any one of the following");
+		System.out.println("1. Manage customer(s)");
+		System.out.println("2. Manage Subscription(s)");
+		System.out.println("3. Manage Deliveries");
+		System.out.println("4. Manage Payment(s)");
+	}
+	
 	private static void listCustomerMenu() {
+		System.out.println("Choose any one of the following");
+		System.out.println("1. Create a customer record");
+		System.out.println("2. View all customer records");
+		System.out.println("3. Update a customer record");
+		System.out.println("4. Delete a customer record");
 		
 	}
 	
     private static void listSubscriptionMenu() {
-		
+    	System.out.println("Choose any one of the following");
+		System.out.println("1. Create a subscription record");
+		System.out.println("2. View all subscription records");
+		System.out.println("3. Update a subscription record");
+		System.out.println("4. Delete a subscription record");
 	}
 
     private static void listDeliveryMenu() {
-	
+    	System.out.println("Choose any one of the following");
+		System.out.println("1. Create a delivery record");
+		System.out.println("2. View all delivery records");
+		System.out.println("3. Update a delivery record");
+		System.out.println("4. Delete a delivery record");
 }
 
     private static void listPaymentMenu() {
-	
+    	System.out.println("Choose any one of the following");
+		System.out.println("1. Create a payment record");
+		System.out.println("2. View all payment records");
+		System.out.println("3. Update a payment record");
+		System.out.println("4. Delete a payment record");
 }
 
+
+	private static boolean printCustomerTable(ResultSet rs) throws Exception {
+		
+		//Print The Contents of the Full Customer Table
+		
+		System.out.println("------------------------------------------------------------------------------------------------------------------------------------");
+		System.out.println("Table: " + rs.getMetaData().getTableName(1));
+		for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+			System.out.printf("%30s",rs.getMetaData().getColumnName(i));
+		}
+		System.out.println();
+		while (rs.next()) {
+			int id = rs.getInt("id");
+			String name = rs.getString("custname");
+			String addr = rs.getString("custaddr");
+			String phone = rs.getString("custphone");
+			System.out.printf("%30s", id);
+			System.out.printf("%30s", name);
+			System.out.printf("%30s", addr);
+			System.out.printf("%30s", phone);
+			System.out.println();
+		}// end while
+		System.out.println("--------------------------------------------------------------------------------------------------------------------------------------");
+		
+		return true;
+		
+	}
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		try {
+			MySQLAccess dao = new MySQLAccess();
+			
+			Scanner keyboard = new Scanner(System.in);
+			String functionNumber = "-99";
+			boolean keepAppOpen = true;
+			
+			while(keepAppOpen == true) {
+				listMainMenu();
+				functionNumber = keyboard.next();
+				
+				switch(functionNumber){
+					case "1":
+						listCustomerMenu();
+						break;
+					case "2":
+						listSubscriptionMenu();
+						break;
+					case "3":
+						listDeliveryMenu();
+						break;
+					case "4":
+						listPaymentMenu();
+						switch(functionNumber) {
+						case "1":
+							System.out.println("Enter Payment ID: \n");
+							String ID = keyboard.next();
+							int payID = Integer.parseInt(ID);
+							System.out.println("Enter Payment Method: \n");
+							String payMethod = keyboard.next();
+							break;
+						case "2":
+							break;
+						case "3":
+							break;
+						case "4":
+							break;
+						default:
+							break;
+						}
+						break;
+					default:
+						break;
+				}
+			}
+			
+		}
+		catch(Exception e) {
+			System.out.println("PROGRAM TERMINATED - ERROR MESSAGE: " + e.getMessage());
+		}
 	}
 
 }
---
+++ NewsagentApplication/src/MySQLAccess.java
@@ -0,0 +1,72 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+public class MySQLAccess {
+	private Connection connect = null;
+	private Statement statement = null;
+	private PreparedStatement preparedStatement = null;
+	private ResultSet resultSet = null;
+	
+	final private String host = "localhost:3306";
+	final private String user = "root";  //Replace with your local MySQL instance credential
+	final private String password = "root";  //Replace with your local MySQL instance credential
+	
+	public MySQLAccess() throws Exception{
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			connect = DriverManager.getConnection("jdbc:mysql://"+ host + "/news_sys?" + "user=" + user + "&password=" + password);
+		}
+		catch(Exception e) {
+			throw e;
+		}
+	}
+	
+	//Add your own methods for CRUD
+	
+	public boolean insertNewPaymentDetail(PaymentHandler p) {
+		boolean insertSuccessfull = true;
+		
+		try {
+			preparedStatement = connect.prepareStatement("INSERT INTO NewsAgent.payment VALUES (?, ?");
+			preparedStatement.setInt(1, p.getID());
+			preparedStatement.setString(2, p.getMethod());
+			preparedStatement.executeUpdate();
+		}
+		catch(Exception e) {
+			insertSuccessfull = false;
+		}
+		
+		return insertSuccessfull;
+	}
+	
+	public ResultSet displayAllPaymentDetails() {
+		try {
+			statement = connect.createStatement();
+			resultSet = statement.executeQuery("SELECT * FROM NewsAgent.payment");
+		}
+		catch(Exception e) {
+			resultSet = null;
+		}
+		return resultSet;
+	}
+	
+	public boolean deletePaymentByID(int payID) {
+		boolean deleteSuccessfull = true;
+		try {
+			if(payID == -99)
+				preparedStatement = connect.prepareStatement("DELETE FROM NewsAgent.payment");
+			else 
+				preparedStatement = connect.prepareStatement("DELETE FROM NewsAgent.payment WHERE ID = " + payID);
+			preparedStatement.executeUpdate();
+		}
+		catch(Exception e) {
+			deleteSuccessfull = false;
+		}
+		return deleteSuccessfull;
+	}
+
+}
