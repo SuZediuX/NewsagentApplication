cce2bd3bd30e648cf5cda47f3bea5b2eb629b194
a00279807 <a00279807@student.ait.ie>
2022-03-01 11:21:27 +0000

    New configuration

--- NewsagentApplication/src/MySQLAccessPaymentTest.java
+++ NewsagentApplication/src/MySQLAccessPaymentTest.java
@@ -2,6 +2,60 @@ import junit.framework.TestCase;
 
 public class MySQLAccessPaymentTest extends TestCase {
 
+	//Test No: 0
+	//Objective: Successfully instantiate MySQLAccess object
+	
+	public void testvalidateMySQLAccess000() {
+		
+		try {
+			MySQLAccess dao = new MySQLAccess();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			fail("Connection not established");
+		}
+	}
+	
 	//Test No: 1
-	//Objective: 
+	//Objective: Push Create statement for Payment successfully
+	
+	public void testvalidateMySQLAccess001() {
+		
+		try {
+			MySQLAccess testHelper = new MySQLAccess();
+			PaymentHandler testObject = new PaymentHandler(200, "Card");
+			
+			boolean result = testHelper.insertNewPaymentDetail(testObject);
+			
+			assertEquals(200, testObject.getID());
+			assertEquals("Card", testObject.getMethod());
+		} catch(Exception e) {
+			fail("Exception not expected!");
+		}
+	}
+	
+	//Test No: 2
+	//Objective: Push Read statement for Payment successfully
+	
+	public void testvalidateMySQLAccess002() {
+		
+		try {
+			MySQLAccess testHelper = new MySQLAccess();
+			
+			
+		} catch(Exception e) {
+			
+		}
+	}
+	
+	//Test No: 3
+	//Objective: Push Update statement for Payment successfully
+	
+	//Test No: 4
+	// Objective: Push Delete statement for Payment successfully
+	
+	//Test No: 5
+	// Objective:
+	
+	//Test No: 6
+	// Objective:
 }
